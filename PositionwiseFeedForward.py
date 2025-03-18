d2dd2cdf70e8e8d911072fd6e0794be58a94f13d
ashtonjur <ashtonjurkowsku@gmail.com>
2025-03-18 13:15:10 +0100

    Create PositionwiseFeedForward.py

---
+++ PositionwiseFeedForward.py
@@ -0,0 +1,9 @@
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, embed_size, ff_hidden_size):
+        super(PositionwiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(embed_size, ff_hidden_size)
+        self.fc2 = nn.Linear(ff_hidden_size, embed_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
