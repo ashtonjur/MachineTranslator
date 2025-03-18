d11ebd3d731494b072337ee42c9bd8ca9c73bc7d
ashtonjur <ashtonjurkowsku@gmail.com>
2025-03-18 13:13:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,19 +38,6 @@ def collate_fn(batch):
 
     return src_batch, tgt_batch
 
-class ScaledDotProductAttention(nn.Module):
-    def __init__(self):
-        super(ScaledDotProductAttention, self).__init__()
-
-    def forward(self, query, key, value, mask=None):
-        scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(query.size(-1))
-        if mask is not None:
-            scores = scores.masked_fill(mask == 0, -1e9)
-        attn_weights = torch.softmax(scores, dim=-1)
-        output = torch.matmul(attn_weights, value)
-        return output, attn_weights
-
-
 class MultiHeadAttention(nn.Module):
     def __init__(self, embed_size, num_heads):
         super(MultiHeadAttention, self).__init__()
