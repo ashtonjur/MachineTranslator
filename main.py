0756f00201e4fd07b0ff93c3e9974f30120ff7c4
ashtonjur <ashtonjurkowsku@gmail.com>
2025-03-18 13:14:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,17 +38,6 @@ def collate_fn(batch):
 
     return src_batch, tgt_batch
 
-class PositionwiseFeedForward(nn.Module):
-    def __init__(self, embed_size, ff_hidden_size):
-        super(PositionwiseFeedForward, self).__init__()
-        self.fc1 = nn.Linear(embed_size, ff_hidden_size)
-        self.fc2 = nn.Linear(ff_hidden_size, embed_size)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        return self.fc2(self.relu(self.fc1(x)))
-
-
 class PositionalEncoding(nn.Module):
     def __init__(self, embed_size, max_len=5000):
         super(PositionalEncoding, self).__init__()
